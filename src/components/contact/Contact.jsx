2dfd126f858ff85665018b4c3bb44892f7a64107
Nischal Acharya <Nischal060@gmail.com>
2024-05-03 08:59:35 +0545

    add map added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,11 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import { BsArrowRight } from "react-icons/bs";
 import { RiSendPlaneFill } from "react-icons/ri";
 import emailjs from '@emailjs/browser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ReCAPTCHA from "react-google-recaptcha";
-import {AiOutlineMail, AiOutlinePhone, AiOutlineHome, AiOutlineMobile} from "react-icons/ai";
+import { AiOutlineMail, AiOutlinePhone, AiOutlineHome, AiOutlineMobile } from "react-icons/ai";
 
 
 const Contact = () => {
@@ -59,17 +59,17 @@ const Contact = () => {
         </div>
         <div
             data-aos="fade-up"
-            className="left flex-1 w-full">
+            className="left flex-1 w-full"
+        >
           <p className="text-gray-700 font-medium w-[100%]">
             Here You can Contact Me.
           </p>
         </div>
         {/* Contact Information Boxes */}
-        <div className="contact-info-boxes" data-aos="fade-up"
-             data-aos-duration="5000">
+        <div className="contact-info-boxes" data-aos="fade-up" data-aos-duration="5000">
           {/* Email Box */}
           <div className="contact-info-box">
-            <AiOutlineMail className="icon" />
+            <AiOutlineMail className="icon"/>
             <div>
               <h3>Email</h3>
               <p>Nischal060@gmail.com</p>
@@ -77,7 +77,7 @@ const Contact = () => {
           </div>
           {/* Phone Number Box */}
           <div className="contact-info-box">
-            <AiOutlineMobile className="icon" />
+            <AiOutlineMobile className="icon"/>
             <div>
               <h3>Phone Number</h3>
               <p>+977-9806081469</p>
@@ -85,89 +85,99 @@ const Contact = () => {
           </div>
           {/* Address Box */}
           <div className="contact-info-box">
-            <AiOutlineHome className="icon" />
+            <AiOutlineHome className="icon"/>
             <div>
               <h3>Address</h3>
               <p>Gauradaha Jhapa, Nepal</p>
             </div>
           </div>
         </div>
+        {/* Google Map */}
+        <div className="w-full mt-5">
+          <iframe
+              className="w-full h-96"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114187.17942643426!2d87.6365509580823!3d26.57316242938736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e59aeb2ac5d359%3A0x8a740efe290d8ed2!2sGauradaha%2057200!5e0!3m2!1sen!2snp!4v1714705717716!5m2!1sen!2snp"
+              allowFullScreen=""
+              loading="lazy"
+          ></iframe>
+        </div>
+        {/* Contact Form */}
         <div className="card-wrapper w-[90%] sm:w-[100%] mx-auto mt-5 flex items-center justify-center sm:flex-col">
-            <form
-                ref={formRef}
-                onSubmit={sendEmail}
-                data-aos="zoom-in"
-                className="flex justify-center items-center flex-col gap-5 w-[70%] md:w-[100%] sm:w-[95%] mx-auto"
-            >
-              <div className="text-left w-full">
-                <label htmlFor="user_email" className="block mb-1 text-gray-600 font-semibold flex items-center">
-                  <span className="material-icons mr-2">email</span>
-                  Email
-                </label>
-                <input
-                    className="px-3 border border-gray-300 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
-                    type="email"
-                    placeholder="Nischal060@gmail.com"
-                    name="user_email"
-                    required
-                />
-              </div>
-              <div className="text-left w-full">
-                <label htmlFor="user_name" className="text-gray-600 font-semibold mb-1 flex items-center">
-                  <span className="material-icons mr-2">person</span>
-                  Name
-                </label>
-                <input
-                    className="px-3 border border-gray-300 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
-                    type="text"
-                    placeholder="Nischal Acharya"
-                    name="user_name"
-                    required
-                />
-              </div>
-              <div className="text-left w-full">
-                <label htmlFor="user_subject" className="text-gray-600 font-semibold mb-1 flex items-center">
-                  <span className="material-symbols-outlined">import_contacts</span>
-                  &nbsp;
-                   Subject
-                </label>
-                <input
-                    className="px-3 border border-gray-300 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
-                    type="text"
-                    placeholder="I need help in this..."
-                    name="user_subject"
-                    required
-                />
-              </div>
-              <div className="text-left w-full">
-                <label htmlFor="message" className="text-gray-600 font-semibold mb-1 flex items-center">
-                  <span className="material-icons mr-2">message</span>
-                  Message
-                </label>
-                <textarea
-                    className="px-3 border border-gray-300 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
-                    rows="4"
-                    cols="50"
-                    placeholder="Write your message"
-                    name="message"
-                    id="message"
-                    required
-                />
-              </div>
-              <ReCAPTCHA
-                  ref={recaptchaRef}
-                  sitekey="6Leu0q4nAAAAAA6B5LZvGfCbM432JKOtvgCtiUCO"
+          <form
+              ref={formRef}
+              onSubmit={sendEmail}
+              data-aos="zoom-in"
+              className="flex justify-center items-center flex-col gap-5 w-[70%] md:w-[100%] sm:w-[95%] mx-auto"
+          >
+            <div className="text-left w-full">
+              <label htmlFor="user_email" className="block mb-1 text-gray-600 font-semibold flex items-center">
+                <span className="material-icons mr-2">email</span>
+                Email
+              </label>
+              <input
+                  className="px-3 border border-gray-300 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
+                  type="email"
+                  placeholder="Nischal060@gmail.com"
+                  name="user_email"
+                  required
               />
+            </div>
+            <div className="text-left w-full">
+              <label htmlFor="user_name" className="text-gray-600 font-semibold mb-1 flex items-center">
+                <span className="material-icons mr-2">person</span>
+                Name
+              </label>
+              <input
+                  className="px-3 border border-gray-300 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
+                  type="text"
+                  placeholder="Nischal Acharya"
+                  name="user_name"
+                  required
+              />
+            </div>
+            <div className="text-left w-full">
+              <label htmlFor="user_subject" className="text-gray-600 font-semibold mb-1 flex items-center">
+                <span className="material-symbols-outlined">import_contacts</span>
+                &nbsp;
+                Subject
+              </label>
+              <input
+                  className="px-3 border border-gray-300 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
+                  type="text"
+                  placeholder="I need help in this..."
+                  name="user_subject"
+                  required
+              />
+            </div>
+            <div className="text-left w-full">
+              <label htmlFor="message" className="text-gray-600 font-semibold mb-1 flex items-center">
+                <span className="material-icons mr-2">message</span>
+                Message
+              </label>
+              <textarea
+                  className="px-3 border border-gray-300 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
+                  rows="4"
+                  cols="50"
+                  placeholder="Write your message"
+                  name="message"
+                  id="message"
+                  required
+              />
+            </div>
+            <ReCAPTCHA
+                ref={recaptchaRef}
+                sitekey="6Leu0q4nAAAAAA6B5LZvGfCbM432JKOtvgCtiUCO"
+            />
 
-              <button
-                  className="bg-violet-700 hover:bg-violet-800 w-full text-white font-semibold p-2 rounded-lg flex items-center justify-center space-x-1"
-                  type="submit"
-              >
-                <span>Send</span>
-                <RiSendPlaneFill />
-              </button>
-              <ToastContainer />
-            </form>
+            <button
+                className="bg-violet-700 hover:bg-violet-800 w-full text-white font-semibold p-2 rounded-lg flex items-center justify-center space-x-1"
+                type="submit"
+            >
+              <span>Send</span>
+              <RiSendPlaneFill/>
+            </button>
+            <ToastContainer/>
+          </form>
         </div>
       </div>
   );
